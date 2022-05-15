5b109ba0dc27f752a92ba48355b0be9c160dc436
Egor_Ostap <shake90@mail.ru>
2022-05-15 13:24:40 +0500

    ..

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def try_ex_dec(fn):
 
     return wrapped
 
-
+# сбор информации по айпи
 @try_ex_dec
 def check_by_ip(source='http://ip-api.com/json/', ip=''):
     response = requests.get(url=source+ip).json()
@@ -19,7 +19,7 @@ def check_by_ip(source='http://ip-api.com/json/', ip=''):
     # to json
     with open('data.json', 'w') as f:
         json.dump(response, f, indent=4, ensure_ascii=False)
-
+    # определения местоположения на карте по широте и долготе
     area = folium.Map(location=[response['lat'], response['lon']])
     area.save(f'{response["query"]}_{response["city"]}.html')
 
