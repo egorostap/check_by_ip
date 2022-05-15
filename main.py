25d41d61fa178886fa9f834eea3d8ebb64b7d471
Egor_Ostap <shake90@mail.ru>
2022-05-15 08:10:39 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import folium
+
+def try_ex_dec(fn):
+    def wrapped(*args, **kwargs):
+        try:
+            fn(*args, **kwargs)
+        except Exception as e:
+            print('ошибка:', e)
+
+    return wrapped
+
+
+@try_ex_dec
+def check_by_ip(source='http://ip-api.com/json/', ip=''):
+    response = requests.get(url=source+ip).json()
+
+    # to json
+    with open('data.json', 'w') as f:
+        json.dump(response, f, indent=4, ensure_ascii=False)
+
+    area = folium.Map(location=[response['lat'], response['lon']])
+    area.save(f'{response["query"]}_{response["city"]}.html')
+
+    print(response)
+    return response
+
+
+if __name__ == '__main__':
+    check_by_ip(ip='185.2.154.85')
+
